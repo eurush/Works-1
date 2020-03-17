9a4336613b511a8d2f5c49b75ca675d076816500
Anuj Tomar <44106871+eurush@users.noreply.github.com>
2020-03-17 19:19:15 +0530

    Update push.bat

--- BAT/Generic Array using BAT scripting/push.bat
+++ BAT/Generic Array using BAT scripting/push.bat
@@ -3,12 +3,12 @@ rem setlocal enabledelayedexpansion
 goto :add_element
 :helper
 	call create_element %~1 %~2 %~3
-	echo bbbb
+	
 goto:eof
 :add_element
 	set %1[%index%]=%2
 	call :helper %1[%index%] %2 %3
-	echo aaaa !index!
+	
 	set /a index= %index% +1
 	rem echo !index!
 
